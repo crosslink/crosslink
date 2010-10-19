c071a8d2da5629c43d162a216ded0645ac6a1e47
lingxiang.tang <lingxiang.tang@304270f0-55b0-358b-3aef-fcebb9074dbc>
2010-10-19 01:28:04 +0000

    fixes for some compilation errors

--- AssessmentEngine/src/ltwassessment/utility/tbaTxtPaneManager.java
+++ AssessmentEngine/src/ltwassessment/utility/tbaTxtPaneManager.java
@@ -1,6 +1,7 @@
 package ltwassessment.utility;
 
 import java.awt.Insets;
+import java.io.File;
 import java.util.Hashtable;
 import java.util.Vector;
 import java.util.logging.Level;
@@ -13,20 +14,20 @@ import javax.swing.text.Highlighter;
 import javax.swing.text.Style;
 import javax.swing.text.StyleConstants;
 import javax.swing.text.StyledDocument;
-import ltwassessment.ltwassessmentView;
+
+import ltwassessment.AppResource;
 import ltwassessment.parsers.FOLTXTMatcher;
 import ltwassessment.parsers.Xml2Html;
 import ltwassessment.parsers.poolerManager;
 import ltwassessment.parsers.resourcesManager;
+import ltwassessment.utility.highlightPainters;
+import ltwassessment.utility.tabTxtPaneManager;
 
 /**
  * @author Darren HUANG
  */
 public class tbaTxtPaneManager {
 
-    private final String sysPropertyKey = "isTABKey";
-    private final String sysPropertyIsTopicWikiKey = "isTopicWikipedia";
-    private final String sysPropertyIsLinkWikiKey = "isLinkWikipedia";
     protected final int bepLength = 4;
     private poolerManager myRunsPooler;
     private resourcesManager myRSCManager;
@@ -57,16 +58,16 @@ public class tbaTxtPaneManager {
     }
 
     public tbaTxtPaneManager() {
-        org.jdesktop.application.ResourceMap resourceMap = org.jdesktop.application.Application.getInstance(ltwassessment.ltwassessmentApp.class).getContext().getResourceMap(ltwassessmentView.class);
-        this.wikipediaCollTitle = resourceMap.getString("collectionType.Wikipedia");
-        this.teAraCollTitle = resourceMap.getString("collectionType.TeAra");
-        this.wikipediaTopicDirectory = resourceMap.getString("wikipedia.topics.folder");
-        this.bepIconImageFilePath = resourceMap.getString("bepIcon.imageFilePath");
-        this.bepHighlightIconImageFilePath = resourceMap.getString("bepHighlightIcon.imageFilePath");
-        this.contentType = resourceMap.getString("html.content.type");
-        this.topicBepsHTPrefix = resourceMap.getString("topicBepsHT.Prefix");
+        //org.jdesktop.application.ResourceMap resourceMap = org.jdesktop.application.Application.getInstance(ltwassessment.ltwassessmentApp.class).getContext().getResourceMap(ltwassessmentView.class);
+        this.wikipediaCollTitle = AppResource.getInstance().getResourceMap().getString("collectionType.Wikipedia");
+        this.teAraCollTitle = AppResource.getInstance().getResourceMap().getString("collectionType.TeAra");
+        this.wikipediaTopicDirectory = AppResource.getInstance().getResourceMap().getString("wikipedia.topics.folder");
+        this.bepIconImageFilePath = AppResource.getInstance().getResourceMap().getString("bepIcon.imageFilePath");
+        this.bepHighlightIconImageFilePath = AppResource.getInstance().getResourceMap().getString("bepHighlightIcon.imageFilePath");
+        this.contentType = AppResource.getInstance().getResourceMap().getString("html.content.type");
+        this.topicBepsHTPrefix = AppResource.getInstance().getResourceMap().getString("topicBepsHT.Prefix");
 
-        this.myRunsPooler = new poolerManager();
+        this.myRunsPooler = poolerManager.getInstance();
         this.myRSCManager = new resourcesManager();
         this.myFOLMatcher = new FOLTXTMatcher();
 
@@ -111,19 +112,19 @@ public class tbaTxtPaneManager {
     // 3) Update Topic Text Pane
     private void updateTopicPaneStatus(String myTopicID) {
         String topicXmlPath = "";
-        if (Boolean.valueOf(System.getProperty(sysPropertyIsTopicWikiKey))) {
+//        if (myRSCManager.getTopicCollType().equals(wikipediaCollTitle)) {
             isTopicWikipedia = true;
             topicXmlPath = wikipediaTopicDirectory + myTopicID + ".xml";
-        } else {
-            isTopicWikipedia = false;
-            String subPath = myRSCManager.getTeAraFilePathByName(myTopicID + ".xml");
-            if (subPath.equals("FileNotFound.xml")){
-                topicXmlPath = "resources\\Tool_Resources\\" + subPath;
-            } else {
-                topicXmlPath = myRSCManager.getTeAraCollectionFolder() + subPath;
-            }
-//            topicXmlPath = myRSCManager.getTeAraCollectionFolder() + myRSCManager.getTeAraFilePathByName(myTopicID + ".xml");
-        }
+//        } else if (myRSCManager.getTopicCollType().equals(teAraCollTitle)) {
+//            isTopicWikipedia = false;
+//            String subPath = myRSCManager.getTeAraFilePathByName(myTopicID + ".xml");
+//            if (subPath.equals("FileNotFound.xml")){
+//                topicXmlPath = "resources" + File.separator + "Tool_Resources" + File.separator + subPath;
+//            } else {
+//                topicXmlPath = myRSCManager.getTeAraCollectionFolder() + subPath;
+//            }
+////            topicXmlPath = myRSCManager.getTeAraCollectionFolder() + myRSCManager.getTeAraFilePathByName(myTopicID + ".xml");
+//        }
         myRSCManager.updateCurrTopicID(topicXmlPath);
         // Set Topic Content Text
         boolean isTopicPane = true;
@@ -131,7 +132,7 @@ public class tbaTxtPaneManager {
         // INSERT BEP Icons
         Hashtable<String, Vector<String[]>> topicBepsHT = myRunsPooler.getTopicAllBeps();
         Vector<String[]> thisTopicBepsV = topicBepsHT.get(topicBepsHTPrefix + myTopicID);
-        Vector<String[]> bepSCRPosV = myFOLMatcher.getBepSCRSPVS(this.myTopicPane, myTopicID, thisTopicBepsV, isTopicWikipedia);
+        Vector<String[]> bepSCRPosV = myFOLMatcher.getSCRBepPosV(this.myTopicPane, myTopicID, thisTopicBepsV, isTopicWikipedia);
         Vector<String> myBepSCROffsetV = new Vector<String>();
         for (String[] bepSCRPos : bepSCRPosV) {
             myBepSCROffsetV.add(bepSCRPos[0]);
@@ -144,7 +145,7 @@ public class tbaTxtPaneManager {
 
     private void updateTopicPaneBepStatus(String topicID, String selectedBepOffset) {
         // currAnchorSCRSet: [0]:Anchor_Name, [1]:Offset, [2]:Offset + Length
-        Vector<String> bepOLListV = myRSCManager.getTopicBepsOSVS();
+        Vector<String> bepOLListV = myRSCManager.getCurrBepsOV();
         String sPos = "0";
         for (String thisOLList : bepOLListV) {
             String[] thisOLSA = thisOLList.split(" : ");
@@ -164,23 +165,23 @@ public class tbaTxtPaneManager {
     private void updateLinkPaneAnchorStatus(String[] anchorOLSA, String rowAnchorFileID) {
         String thisFileID = rowAnchorFileID;
         String linkXmlPath = "";
-        if (Boolean.valueOf(System.getProperty(sysPropertyIsTopicWikiKey))) {
+//        if (myRSCManager.getLinkCollType().equals(wikipediaCollTitle)) {
             isLinkWikipedia = true;
             String subPath = myRSCManager.getWikipediaFilePathByName(thisFileID + ".xml");
             if (subPath.equals("FileNotFound.xml")){
-                linkXmlPath = "resources\\Tool_Resources\\" + subPath;
+                linkXmlPath = "resources" + File.separator + "Tool_Resources" + File.separator + subPath;
             } else {
                 linkXmlPath = myRSCManager.getWikipediaCollectionFolder() + subPath;
             }
-        } else {
-            isLinkWikipedia = false;
-            String subPath = myRSCManager.getTeAraFilePathByName(thisFileID + ".xml");
-            if (subPath.equals("FileNotFound.xml")){
-                linkXmlPath = "resources\\Tool_Resources\\" + subPath;
-            } else {
-                linkXmlPath = myRSCManager.getTeAraCollectionFolder() + subPath;
-            }
-        }
+//        } else if (myRSCManager.getLinkCollType().equals(teAraCollTitle)) {
+//            isLinkWikipedia = false;
+//            String subPath = myRSCManager.getTeAraFilePathByName(thisFileID + ".xml");
+//            if (subPath.equals("FileNotFound.xml")){
+//                linkXmlPath = "resources" + File.separator + "Tool_Resources" + File.separator + subPath;
+//            } else {
+//                linkXmlPath = myRSCManager.getTeAraCollectionFolder() + subPath;
+//            }
+//        }
         boolean isTopicPane = false;
         setTextPaneContent(linkXmlPath, isTopicPane);
     }
@@ -201,7 +202,7 @@ public class tbaTxtPaneManager {
         try {
             StyledDocument styDoc = (StyledDocument) txtPane.getDocument();
             Vector<String> HBepSCROffsetV = new Vector<String>();
-            Vector<String> bepOLListV = myRSCManager.getTopicBepsOSVS();
+            Vector<String> bepOLListV = myRSCManager.getCurrBepsOV();
             for (String thisBepOL : bepOLListV) {
                 String[] thisBepOLSA = thisBepOL.split(" : ");
                 String thisBEPTxt = styDoc.getText(Integer.valueOf(thisBepOLSA[1]), bepLength);
@@ -265,7 +266,7 @@ public class tbaTxtPaneManager {
         this.myLinkPane.setText(xmlParser.getHtmlContent().toString());
         this.myLinkPane.setCaretPosition(0);
         try {
-            String[] mySCRAnchorSEPos = myFOLMatcher.getSCRAnchorNameSESA(myLinkPane, rowAnchorFileID, new String[]{rowAnchorXmlOffset, rowAnchorXmlLength, ""});
+            String[] mySCRAnchorSEPos = myFOLMatcher.getSCRAnchorPosSA(myLinkPane, rowAnchorFileID, new String[]{rowAnchorXmlOffset, rowAnchorXmlLength, ""});
             Highlighter txtPaneHighlighter = this.myLinkPane.getHighlighter();
             int[] achorSCRPos = new int[]{Integer.valueOf(mySCRAnchorSEPos[1]), Integer.valueOf(mySCRAnchorSEPos[2])};
             Object anchorHighlightRef = txtPaneHighlighter.addHighlight(achorSCRPos[0], achorSCRPos[1], painters.getAnchorPainter());
