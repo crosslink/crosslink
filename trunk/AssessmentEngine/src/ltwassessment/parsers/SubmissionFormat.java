90f062984c1e31b97a11dc0a48ce3d7f1c7e54d8
lingxiang.tang <lingxiang.tang@304270f0-55b0-358b-3aef-fcebb9074dbc>
2010-10-28 05:35:16 +0000

    re-adjust the anchor offset

---
+++ trunk/AssessmentEngine/src/ltwassessment/parsers/SubmissionFormat.java
@@ -0,0 +1,15 @@
+package ltwassessment.parsers;
+
+public class SubmissionFormat {
+	public static final String OUTGOING_TAG_NAME = "outgoing";
+	public static final String OUTGOINGLINKS_TAG_NAME = "outgoinglinks";
+	
+	public static final String afTopicTag = "topic";
+	public static final String afOutgoingTag = "outgoing";
+    public static final String afAnchorTag = "anchor";
+    public static final String afSubAnchorTag = "subanchor";
+    public static final String afToBepTag = "tofile";
+    public static final String offsetAttributeName = "offset";
+    public static final String lengthAttributeName = "length";
+    public static final String tboffsetAttributeName = "bep_offset";
+}
