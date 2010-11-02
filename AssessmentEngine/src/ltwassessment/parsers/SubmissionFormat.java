ee8b7ac5b4472926efed34a39c3296362651d142
lingxiang.tang <lingxiang.tang@304270f0-55b0-358b-3aef-fcebb9074dbc>
2010-11-02 00:56:34 +0000

    adjust code in Submission Format

--- AssessmentEngine/src/ltwassessment/parsers/SubmissionFormat.java
+++ AssessmentEngine/src/ltwassessment/parsers/SubmissionFormat.java
@@ -12,4 +12,57 @@ public class SubmissionFormat {
     public static final String offsetAttributeName = "offset";
     public static final String lengthAttributeName = "length";
     public static final String tboffsetAttributeName = "bep_offset";
+    
+	private SubmissionFormat instance = null;
+	
+	
+	public SubmissionFormat() {
+
+	}
+	
+	public static String getOutgoingTag() {
+		return OUTGOING_TAG_NAME;
+	}
+	
+	public static String getOutgoingLinksTag() {
+		return OUTGOINGLINKS_TAG_NAME;
+	}
+	
+	public static String getAftopictag() {
+		return afTopicTag;
+	}
+
+	public static String getAfoutgoingtag() {
+		return afOutgoingTag;
+	}
+
+	public static String getAfanchortag() {
+		return afAnchorTag;
+	}
+
+	public static String getAfsubanchortag() {
+		return afSubAnchorTag;
+	}
+
+	public static String getAftobeptag() {
+		return afToBepTag;
+	}
+
+	public static String getOffsetattributename() {
+		return offsetAttributeName;
+	}
+
+	public static String getLengthattributename() {
+		return lengthAttributeName;
+	}
+
+	public static String getTboffsetattributename() {
+		return tboffsetAttributeName;
+	}
+
+	public SubmissionFormat getInstance() {
+		if (instance == null)
+			instance = new SubmissionFormat();
+		return instance;
+	}
 }
