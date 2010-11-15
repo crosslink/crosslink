f116fe32a620cfc0d1c7b089d6e9099ced5f4bfa
lingxiang.tang <lingxiang.tang@304270f0-55b0-358b-3aef-fcebb9074dbc>
2010-11-15 05:36:10 +0000

    re-configure the GUI of validator

--- AssessmentEngine/src/ltwassessment/pool/Target.java
+++ AssessmentEngine/src/ltwassessment/pool/Target.java
@@ -9,24 +9,31 @@ public class Target {
 	public String getLang() {
 		return lang;
 	}
+	
 	public void setLang(String lang) {
 		this.lang = lang;
 	}
+	
 	public String getTitle() {
 		return title;
 	}
+	
 	public void setTitle(String title) {
 		this.title = title;
 	}
+	
 	public String getId() {
 		return id;
 	}
+	
 	public void setId(String id) {
 		this.id = id;
 	}
+	
 	public int getBepOffset() {
 		return bepOffset;
 	}
+	
 	public void setBepOffset(int bepOffset) {
 		this.bepOffset = bepOffset;
 	}
