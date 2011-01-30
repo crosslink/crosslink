2542abac0856e7273ac931ea1f7cad02a7888644
lingxiang.tang <lingxiang.tang@304270f0-55b0-358b-3aef-fcebb9074dbc>
2011-01-30 03:14:47 +0000

    updated the run scripts

--- CrosslinkValidation/run.bat
+++ CrosslinkValidation/run.bat
@@ -1,5 +1,5 @@
 
 java -cp .;lib\xml-apis.jar;lib\xml2txt.jar;lib\xercesImpl.jar;lib\vtd-xml.jar;lib\swing-worker-1.2.jar;lib\resolver.jar;lib\org.eclipse.swt.win32.win32.x86_3.3.0.v3346.jar;lib\jdom.jar;lib\assessment-engine.jar;lib\appframework-1.0.3.jar
- ltwassessmenttool.LTWAssessmentToolApp
+ -jar CrosslinkValidation.jar
 
 
