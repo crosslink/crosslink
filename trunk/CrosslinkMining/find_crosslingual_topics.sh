57db431e7421dc41a5246fa972d4c1178dbb14a1
lingxiang.tang <lingxiang.tang@304270f0-55b0-358b-3aef-fcebb9074dbc>
2011-03-08 04:41:25 +0000

    updated the link mining script

--- trunk/CrosslinkMining/find_crosslingual_topics.sh
+++ trunk/CrosslinkMining/find_crosslingual_topics.sh
@@ -15,7 +15,13 @@ cl_table_path=$3
 
 corpus_path=$4
 
-if [ -e $cl_table_path ]
+####################################################################
+#                       Sample usage
+####################################################################
+# ./find_crosslingual_topics.sh ~/experiments/c2e-ld/zh-topics ~/experiments/c2e-ld/en-topics ~/experiments/ntcir-9-clld/assessment/wikipedia_groundtruth/link-mining/zh-en/c2e_table.txt /data/corpus/wikipedia/enwiki/en/
+
+
+if ! [ -e $cl_table_path ]
 then
 	echo "the cross-lingual link table: \"$cl_table_path\" does't not exist"
 	exit
@@ -24,6 +30,8 @@ fi
 for file in `ls $topic_path`
 do
 	source_id=`echo $file | cut -f 1 -d .`
+	echo "processing $file: \"$source_id\""
+	    
 	line=`grep ^$source_id: $cl_table_path`
 	
 	if [ "$?" -eq "0" ]
@@ -50,5 +58,7 @@ do
 		filename="pages/$filename"
 		#; cat $filename | head -n 50
 		echo "cp $corpus_path/$filename $cp_topic_path"
+		cp -v $corpus_path/$filename $cp_topic_path
 	fi
+	echo 
 done
