12bb29acdaf944a5e3367c598d355170e3696cb0
lingxiang.tang <lingxiang.tang@304270f0-55b0-358b-3aef-fcebb9074dbc>
2011-03-11 02:06:29 +0000

    made CrossMining a java project and added initial code

---
+++ CrosslinkMining/src/crosslink/CrosslinkTable.java
@@ -0,0 +1,19 @@
+package crosslink;
+
+public class CrosslinkTable {
+	private String tablePath;
+
+	public CrosslinkTable(String tablePath) {
+		super();
+		this.tablePath = tablePath;
+	}
+
+	public void read() {
+		
+	}
+
+	public int getTargetCount(String pagePath) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
