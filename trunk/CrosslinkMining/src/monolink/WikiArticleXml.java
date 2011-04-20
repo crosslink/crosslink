4fed18b9a8f19118566fa17193efc39682642cb0
lingxiang.tang <lingxiang.tang@304270f0-55b0-358b-3aef-fcebb9074dbc>
2011-04-20 03:18:28 +0000

    bugs fixes in Crosslink tools and library

--- trunk/CrosslinkMining/src/monolink/WikiArticleXml.java
+++ trunk/CrosslinkMining/src/monolink/WikiArticleXml.java
@@ -1,6 +1,13 @@
 package monolink;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.Document;
@@ -14,6 +21,8 @@ public class WikiArticleXml {
 
 	private String title;
 	private String id;
+	
+	protected int currentPos = 0;
 	
 	/**
 	 * @return the title
@@ -37,6 +46,34 @@ public class WikiArticleXml {
 	}
 	
 	public void read() {
+		String filename =  new File(xmlFile).getName();
+		id = filename.substring(0, filename.indexOf('.'));
+		int pos;
+    	try {
+			BufferedReader br = new BufferedReader(
+			        new InputStreamReader(new FileInputStream(xmlFile), "UTF-8"));
+			String strLine;
+			while ((strLine = br.readLine()) != null) {
+			    if ((pos = strLine.indexOf("<title>")) > -1) {
+			    	pos += 7;
+			    	title = strLine.substring(pos, strLine.indexOf('<', pos));
+			    	break;
+			    }
+			}
+			br.close();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void readXml() {
 		try {
 			File file = new File(xmlFile);
 			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
