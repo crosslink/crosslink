d6b787c0ef80706bdab60d09f7b3fe2d30744529
lingxiang.tang <lingxiang.tang@304270f0-55b0-358b-3aef-fcebb9074dbc>
2011-05-20 02:06:36 +0000

    updated the scripts to remove/recover CJK counterparts

--- trunk/CrosslinkTopic/removeCJKTopics.sh
+++ trunk/CrosslinkTopic/removeCJKTopics.sh
@@ -1,10 +1,39 @@
 #!/bin/bash
+#
+# version: 1.0
+#
 
-# Corresponding Chinese Topics
-EN_C_TOPICS=""
+HOME_PATH=`dirname $0`
 
-# Corresponding Japanese Topics
-EN_J_TOPICS=""
+langs="zh"
+CJK_CORPORA_PATH=$1
 
-# Corresponding Korean Topics
-EN_K_TOPICS=""
+if ! [ -n "$CJK_CORPORA_PATH" ]; then
+	echo "Usage: $0 [CJK document collections path]"
+	exit -1
+fi
+
+if ! [ -e "$CJK_CORPORA_PATH" ]; then
+	echo "Error: $CJK_CORPORA_PATH does not exist"
+	exit -2
+fi
+
+REMOVED_TOPICS_PATH=$CJK_CORPORA_PATH/topics
+
+mkdir 
+
+for i in $langs
+do
+	echo "removing $i topics from corpus..."
+	TOPICS=$HOME_PATH/${i}_topics_list.txt
+	
+	if [ -e "$TOPICS" ]; then
+		for topic in `cat $TOPICS`
+		do
+			echo $topic
+			
+		done
+	else
+		echo "Error: no $TOPICS found"
+	fi
+done
