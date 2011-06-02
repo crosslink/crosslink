a0d27cf0985a06ab247f7cff2777834c28903ba7
lingxiang.tang <lingxiang.tang@304270f0-55b0-358b-3aef-fcebb9074dbc>
2011-06-02 07:35:02 +0000

    updated the pooling for split the submissions according the topic

--- AssessmentEngine/src/ltwassessment/pool/Pool.java
+++ AssessmentEngine/src/ltwassessment/pool/Pool.java
@@ -1,6 +1,10 @@
 package ltwassessment.pool;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
@@ -24,7 +28,7 @@ public class Pool {
 			runs.read(file);
 	}
 	
-	public String output() {
+	public void output(boolean splitTopic) {
 		StringBuffer poolXml = new StringBuffer();
 		
 		ToXml.startRootElement(poolXml);
@@ -37,8 +41,30 @@ public class Pool {
 	    	Map.Entry me = (Map.Entry)i.next();
 	    	Topic topic = (Topic)me.getValue();
 			ToXml.topicToXml(topic, poolXml);
+			
+			if (splitTopic) {
+				ToXml.endRootElement(poolXml);
+				output2File(poolXml, "wikipedia_pool_" + topic.getId() + ".xml");
+				poolXml.setLength(0);
+				ToXml.startRootElement(poolXml);
+			}
 	    }
 		ToXml.endRootElement(poolXml);
-		return poolXml.toString();
+		
+		if (!splitTopic)
+			System.out.println(poolXml);
+//		return poolXml.toString();
+	}
+	
+	public void output2File(StringBuffer xmlFile, String filename) {
+	            try {
+	                BufferedWriter out = new BufferedWriter(new OutputStreamWriter(
+	        		        new FileOutputStream(filename), "UTF-8")); //new BufferedWriter(new FileWriter(textfilename));
+	        	            out.write(xmlFile.toString());
+					out.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 	}
 }
