fcf6f3802223af9072b21e1db6be440acf14d4fa
lingxiang.tang <lingxiang.tang@304270f0-55b0-358b-3aef-fcebb9074dbc>
2011-07-04 06:31:02 +0000

    minor fixes for showing wrong anchors

--- AssessmentEngine/src/ltwassessment/assessment/AssessmentThread.java
+++ AssessmentEngine/src/ltwassessment/assessment/AssessmentThread.java
@@ -169,7 +169,8 @@ public class AssessmentThread extends Thread {
 					break;
 				}
 				try {
-					Thread.sleep(1000);
+					Thread.sleep(100);
+//					Thread.sleep(1000);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
