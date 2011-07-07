72f087197d4f5b74a1d51fb0bb12f66e2f463e2c
lingxiang.tang <lingxiang.tang@304270f0-55b0-358b-3aef-fcebb9074dbc>
2011-07-07 02:38:24 +0000

    added a connection function for assessment lang

--- trunk/AssessmentEngine/src/ltwassessment/Assessment.java
+++ trunk/AssessmentEngine/src/ltwassessment/Assessment.java
@@ -1,8 +1,13 @@
 package ltwassessment;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Iterator;
@@ -33,6 +38,8 @@ public class Assessment {
 	private static File poolDirHandler = null;
 	private static File poolBackupDirHandler = null;
 	
+	private static String assessmentLang = "";
+	
 	static {
 		poolDirHandler = new File(ASSESSMENT_POOL_PATH);
 		if (!poolDirHandler.exists())
@@ -41,6 +48,8 @@ public class Assessment {
 		poolBackupDirHandler = new File(ASSESSMENT_POOL_BACKUP_DIR);
 		if (!poolBackupDirHandler.exists())
 			poolBackupDirHandler.mkdir();
+		
+		getAssessmentLang();
 	}
 
 	public Assessment() {
@@ -48,6 +57,24 @@ public class Assessment {
 	}
 
 	
+	private static void getAssessmentLang() {
+	    try { 
+	        URL url = new URL("http://131.181.88.158/lang.txt"); 
+
+	        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+	        conn.connect();
+	        InputStreamReader in = new InputStreamReader(conn.getInputStream());
+	        BufferedReader buff = new BufferedReader(in);
+	        String line;
+	        line = buff.readLine();
+	        assessmentLang = line.trim();
+	    } catch(Exception e) { 
+	        throw new RuntimeException(e); 
+	    } 
+		
+	}
+
+
 	public static Assessment getInstance() {
 		if (instance == null)
 			instance = new Assessment(); 
@@ -102,7 +129,9 @@ public class Assessment {
 	}
 	
 	public static String getPoolFile(String id) {
-		return ASSESSMENT_POOL_PATH + ASSESSMENT_POOL_PREFIX + "_" + id + ".xml";
+		if (assessmentLang.length() == 0)
+			return ASSESSMENT_POOL_PATH + ASSESSMENT_POOL_PREFIX + "_" + id + ".xml";
+		return ASSESSMENT_POOL_PATH + assessmentLang + File.separator + ASSESSMENT_POOL_PREFIX + "_" + id + ".xml";
 	}
 	
 	public String getNextTopic() {
