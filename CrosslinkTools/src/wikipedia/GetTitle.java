4900ae9b45136d660cfabc9336a7f276b49192dc
lingxiang.tang <lingxiang.tang@304270f0-55b0-358b-3aef-fcebb9074dbc>
2012-02-02 04:40:36 +0000

    more assessment

---
+++ CrosslinkTools/src/wikipedia/GetTitle.java
@@ -0,0 +1,21 @@
+package wikipedia;
+
+import java.io.File;
+
+import ltwassessment.wiki.WikiArticleXml;
+
+public class GetTitle {
+	public static void usage() {
+		System.err.println("Usage: program /a/path/to/a/wikipedia/article");
+		System.exit(-1);
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		if (args.length <=0)
+			usage();
+		System.out.println(new WikiArticleXml(new File(args[0])).getTitle());
+	}
+}
