06cddb8ef8f608df10c7fa1c29bcd82600035648
lingxiang.tang@gmail.com <lingxiang.tang@gmail.com@304270f0-55b0-358b-3aef-fcebb9074dbc>
2012-02-22 02:27:25 +0000

    add a stack class for list wildcard files

---
+++ trunk/AssessmentEngine/src/ltwassessment/utility/WildcardFileStack.java
@@ -0,0 +1,12 @@
+package ltwassessment.utility;
+
+import java.io.File;
+import java.util.Stack;
+
+public class WildcardFileStack extends WildcardFiles {
+	private Stack<File> stack;
+	
+	public File next() {
+		
+	}
+}
