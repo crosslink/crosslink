44123c2f5b15e5d0fcbb59fdaa9ddef0d4d7af61
lingxiang.tang@gmail.com <lingxiang.tang@gmail.com@304270f0-55b0-358b-3aef-fcebb9074dbc>
2012-08-13 05:03:55 +0000

    a minor bug fixed for getting the topic path right

--- trunk/CrosslinkMining/Crosslink2LinkMining.sh
+++ trunk/CrosslinkMining/Crosslink2LinkMining.sh
@@ -22,6 +22,7 @@ append_jars_to_cp() {
 append_jars_to_cp $base/lib
 
 exec="java -cp $cp:$base/bin crosslink.CrosslinkMining" 
+echo "Program: $exec"
 
 # Chinese to English
 
@@ -29,20 +30,61 @@ for lang in zh ja ko
 do
 	source=$lang
 	target=en
+	
+	if [ "$source" == "zh" ]
+	then
+	  S_CODE="C"
+	fi
+	if [ "$source" == "en" ]
+	then
+	  S_CODE="E"
+	fi
+	if [ "$source" == "ja" ]
+	then
+	  S_CODE="J"
+	fi
+	if [ "$source" == "ko" ]
+	then
+	  S_CODE="K"
+	fi
+	
+	if [ "$target" == "zh" ]
+	then
+	  T_CODE="C"
+	fi
+	if [ "$target" == "en" ]
+	then
+	  T_CODE="E"
+	fi
+	if [ "$target" == "ja" ]
+	then
+	  T_CODE="J"
+	fi
+	if [ "$target" == "ko" ]
+	then
+	  T_CODE="K"
+	fi
+
 	$exec  $source:$target \
 	 /users/tables/all \
-	~/experiments/ntcir-10-crosslink/topics/all-local/${source} \
-	~/experiments/ntcir-10-crosslink/topics/all-local/${target} \
-	/data/corpus/wikipedia/all/
+	~/experiments/ntcir-10-crosslink/topics/with-links/${source} \
+	~/experiments/ntcir-10-crosslink/topics/with-links/${target} \
+	/data/corpus/wikipedia/all/ > A2FWikiGroundTruthResultSet-${S_CODE}2${T_CODE}.xml
 
 	source=en
 	target=$lang
 	
+	TMP_CODE=$S_CODE
+	S_CODE=$T_CODE
+	T_CODE=$TMP_CODE
+	
 	$exec  $source:$target \
 	 /users/tables/all \
-	~/experiments/ntcir-10-crosslink/topics/all-local/${source} \
-	~/experiments/ntcir-10-crosslink/topics/all-local/${target} \
-	/data/corpus/wikipedia/all/
+	~/experiments/ntcir-10-crosslink/topics/with-links/${source} \
+	~/experiments/ntcir-10-crosslink/topics/with-links/${target} \
+	/data/corpus/wikipedia/all/  > A2FWikiGroundTruthResultSet-${S_CODE}2${T_CODE}.xml
+	
+done
 # English to Chinese
 
 # English to Japanese
