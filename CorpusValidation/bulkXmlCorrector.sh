f90898ee6de379bab0d13843f395d310b375d4ec
lingxiang.tang@gmail.com <lingxiang.tang@gmail.com@304270f0-55b0-358b-3aef-fcebb9074dbc>
2013-03-14 02:58:48 +0000

    use the wildcard file class from assessment-engine library

--- CorpusValidation/bulkXmlCorrector.sh
+++ CorpusValidation/bulkXmlCorrector.sh
@@ -13,8 +13,10 @@ corpus_home=$1
 target_path=$2
  
 LOG_FILE=${cwd}/errorfiles.log
+LOG_FILE_BAD=${cwd}/undonefiles.log
 
 \rm -rf $LOG_FILE
+\rm -rf $LOG_FILE_BAD
 
 if [ -z "$corpus_home" ]
 then
@@ -30,6 +32,7 @@ fi
 
 corpus_path_len=`echo ${#corpus_home}`
 
+tmp_good=/tmp/temp_good.xml
 cd $corpus_home
 find . -name "*.xml" | while read line
 do
@@ -37,15 +40,23 @@ do
   name=`basename $line`
   dest_path=${target_path}/${sub_path}
   mkdir -p ${dest_path}
-  ${base}/runValidator.sh  $corpus_home/$line
+  source_file=$corpus_home/$line
+  ${base}/runValidator.sh  $source_file
   if [ "$?" -eq "0" ]
   then
     cp -uv $line ${dest_path}
   else
+    ls ${source_file} >>$LOG_FILE
     # https://github.com/crosslink/xml_corrector
     dest_file=${dest_path}/${name}
-    xml_corrector $line >${dest_file}
-    ls ${dest_file} >>$LOG_FILE
+    xml_corrector $source_file >${tmp_good}
+    ${base}/runValidator.sh ${tmp_good}
+    if [ "$?" -eq "0" ]
+    then
+      mv -f ${tmp_good} ${dest_file}
+    else
+      echo ${source_file} >$LOG_FILE_BAD  
+    fi
   fi  
 done
 
